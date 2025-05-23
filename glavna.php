1760b938d04baab5ed47b7a88691466990041df8
janaccc <jantopler25@gmail.com>
2025-05-23 10:25:51 +0200

    added main page

---
+++ glavna.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="sl">
+<head>
+    <link rel="stylesheet" type="text/css" href="glavna.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music player</title>
+</head>
+<body>
+<section id="predvajalnik">
+    <audio controls>
+        <source src="" type="audio">
+        <source src="" type="audio/">
+        Your browser does not support the audio element.
+    </audio>
+</section>
+</body>
+</html>
+
