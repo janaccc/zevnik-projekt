9c81701cebbc29f217a73f60bae76296a7b2e73a
janaccc <jantopler25@gmail.com>
2025-06-05 13:10:36 +0200

    design

--- registracija.php
+++ registracija.php
@@ -1,1 +1,27 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="sl">
+<head>
+    <link rel="stylesheet" type="text/css" href="index.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registriraj kot nov uporabnik</title>
+</head>
+<body>
+<section id="section">
+    <h1>Registracija</h1>
+    <p id="prvi">
+        Vnesi podatke
+    </p>
+    <form action="" method="POST">
+        Uporabniško ime: <input type="text" name="naziv" value="" required class="vnos" placeholder="Vnesi uporabniško ime"><br>
+        Geslo: <input type="password" name="tip" value="" required class="vnos" placeholder="Vnesi geslo"><br>
+        <div>
+            <input type="submit" name="registracija" value="Registracija" id="posljigumb">
+        </div>
+    </form>
+</section>
+</body>
+</html>
+
