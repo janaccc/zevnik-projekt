190ce5fa197c50ccb89324f03046f44e044ea829
janaccc <jantopler25@gmail.com>
2025-06-05 14:20:14 +0200

    added example povezava

---
+++ povezava_primer.php
@@ -0,0 +1,15 @@
+<?php
+More actions
+/*
+povezava na streznik
+*/
+
+$host='localhost';
+$user='root';
+$password='root';
+$database='database';
+
+$link=mysqli_connect($host, $user, $password, $database)
+or die("Povezovanje ni mogoče");
+mysqli_set_charset($link, "utf8");
+?>
